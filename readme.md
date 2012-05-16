293a9fb6a3fd8f72efecf9b759b9415fdf5c07d2
Joao Oliveira <joaoxsouls@gmail.com>
2012-05-16 18:33:56 +0100

    add draggable event

---
+++ readme.md
@@ -0,0 +1,3 @@
+todolist
+
+uses backbone 0.9.2, backbone-storage and gets geolocation from client
--- script/todolist.js
+++ script/todolist.js
@@ -1,132 +1,161 @@
-window.onload =function() {
-    var todo = Backbone.Model.extend({
-	
+window.onload = function() {
+    //get browser coords
+    navigator.geolocation.getCurrentPosition(function(loc) {
+        window.geo = {
+            'latitude': loc.coords.latitude,
+            'longitude': loc.coords.longitude
+        };
+    });
+
+    var Todo = Backbone.Model.extend({
 	defaults: {
-	    id : "random",
-	    content:"blank",
-	    geolocation:{latitude:0,longitude:0},
-	    done: false
+	    done: false,
+            geolocation: window.geo
+        },
+
+	toggle: function() {
+	    this.set({done: !this.get("done")});
 	},
-	initialize:function(){
-	    if (!this.get("geolocation")) {
-		this.set({"geolocation": this.defaults.geolocation});
-	    }
-	},
-	toggle:function(){
-	    this.set({done: !this.get("done")})
-	},
+
 	clear: function() {
 	    this.destroy();
-	},
-	store:function(){	    
 	}
-    })
-    var todolist = Backbone.Collection.extend({
-	model:todo,
+    });
+
+
+    var TodoSet = Backbone.Collection.extend({
+	model: Todo,
+
 	localStorage: new Backbone.LocalStorage("todolist"),
-	initialize:function(){
-	    
-	},
+
 	done: function() {
 	    return this.filter(function(todo){ return todo.get('done'); });
 	},
-	remaining: function() {
-	    return this.without.apply(this, this.done());
-	},
-	nextOrder: function() {
-	    if (!this.length) return 1;
-	    return this.last().get('id') + 1;
-	},
+
 	comparator: function(todo) {
 	    return todo.get('id');
-	} 
-    })
-    //instanciar a coleção para puder ser usada no view
-    var Todolist = new todolist;
-    var geo;
-    var todoview = Backbone.View.extend({
+	}
+    });
+
+    //collection needs to be instantiated to be used by view
+    Todo.collection = new TodoSet;
+
+    var TodoView = Backbone.View.extend({
 	tagName: 'li',
-	//template: _.template($('#item-template').html()),
+
 	events: {
-	    "click .check" : "toggleDone"},
-	initialize: function(){
-	    //observers do view no mode, quando model remove, o view tambem vai remover
+            "click .check" : "toggleDone"
+        },
+
+	initialize: function() {
+	    //view observers on the model
 	    _.bindAll(this, 'render', 'close', 'clear');
 	    this.model.on('destroy', this.clear);
 	},
+
 	render: function() {
 	    var template= _.template($('#item-template').html());
 	    this.$el.html(template(this.model.toJSON()));
 	    //this.input = this.$('.todo-input');
-	    return this
+	    return this;
 	},
+
 	close: function() {
-	    this.model.save({content: this.input.val()});	
+	    this.model.save({content: this.input.val()});
 	},
+
 	toggleDone: function() {
-	    this.model.clear();
+	    this.model.toggle();
 	},
-	edit : function(){
+
+	edit: function() {
 	    this.input.focus();
 	},
-	clear:function(){
+
+	clear: function() {
 	    this.remove();
 	    this.unbind();
 	}
-    })
-    var Appview = Backbone.View.extend({
-	el:$("#todo-app"),
+    });
+
+
+    var TodoList = Backbone.View.extend({
+	el:$("#container"),
+
 	events:{
 	    "keypress #todo-input" : "createOnEnter",
-	    "click #plus": "createOnClick"},
-	initialize:function(){
-	    //observers do view principal ao model
-	    _.bindAll(this, 'addOne','addAll');
-	    Todolist.on('reset',   this.addAll);
-	    Todolist.on('add',this.addOne);
-	    Todolist.fetch();
+	    "click #plus": "createOnClick",
+	    "click #done .clearBtn": "clearDoneTasks"},
+
+	initialize: function(){
+	    //Appview observers to the collection
+	    _.bindAll(this);
+	    this.collection.on('reset', this.addAll);
+	    this.collection.on('add',this.addOne);
+	    this.collection.on('add',this.checkCompleted);
+	    this.collection.on('remove',this.checkCompleted);
+	    this.collection.on('change:done', this.checkCompleted);
+	    this.collection.fetch();
+            $(this.el).draggable();
 	},
-	createOnEnter:function(e){
+
+	createOnEnter: function(e) {
 	    if (e.keyCode != 13) return;
-	    this.createtodo(this.$("#todo-input").val());
+	    this.createTodo(this.$("#todo-input").val());
 	},
-	createOnClick:function(){
-	    this.createtodo(this.$("#todo-input").val());
+
+	createOnClick: function() {
+	    this.createTodo(this.$("#todo-input").val());
 	},
-	getstats:function(){
-	    var id = Todolist.nextOrder();
-	    var content = this.$("#todo-input").val();
-	    done = false
-	    return {id:id,geolocation:window.geo,content:content};
+
+	addOne: function(model) {
+	    var todo = new TodoView({model: model});
+	    this.$("#todo-list").append(todo.render().el);
+	    this.checkCompleted();
 	},
-	addOne: function(todo) {
-	    var view = new todoview({model: todo});
-	    this.$("#todo-list").append(view.render().el);
+
+	addAll: function(){
+	    this.collection.each(this.addOne);
 	},
-	addAll:function(){
-	    Todolist.each(this.addOne);
-	},
-	createtodo:function(content){
-	    if (content==""){return};
-	    var inputdata = {id: Todolist.nextOrder(),content:content,geolocation:window.geo}
-	    Todolist.create(inputdata);
+
+	createTodo: function(content){
+	    if (content == "")
+                return;
+	    this.collection.create({content: content});
 	    this.$("#todo-input").val("");
-	}	
-    })
-    var App;   
-    //obter coordenadas do browser
-    navigator.geolocation.getCurrentPosition(function(loc){window.geo ={'latitude':loc.coords.latitude,'longitude':loc.coords.longitude}});
-    
-    App = new Appview({el:$("#todo-app")});
-    
-    //router da aplicação
+	},
+
+	checkCompleted: function(){
+            var done = this.collection.done();
+	    if (done.length >0){
+                this.$('#done .count').html(done.length);
+		this.$("#done").show();}
+	    else
+                this.$("#done").hide();
+	},
+
+	clearDoneTasks: function(){
+	    _.each(this.collection.done(), function(todo){
+                todo.clear();
+            });
+	}
+    });
+
+    var todoList = new TodoList({
+        el:$("#todo-app"),
+        collection: Todo.collection
+    });
+
+    //app router
     var appRouter = Backbone.Router.extend({
 	routes: {"add/:query": "addTodo"},
-	addTodo:function(query){
+
+	addTodo: function(query){
 	    App.createtodo(query);
 	}
+    });
 
-    })
     var approutes = new appRouter;
-    Backbone.history.start() 
+
+    Backbone.history.start();
 };
